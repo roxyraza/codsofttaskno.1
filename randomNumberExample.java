bfa7707c7e74161ee4c8f25f5d718d611d894054
roxyraza <razaking581@gmail.com>
2024-09-09 15:02:35 +0530

    Add files via upload

---
+++ randomNumberExample.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class randomNumberExample {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int min = 1;
+        int max = 100;
+
+        int randomNumber = random.nextInt((max - min) + 1) + min;
+        
+        System.out.println("Random number between " + min + " and " + max + ": " + randomNumber);
+    }
+}
